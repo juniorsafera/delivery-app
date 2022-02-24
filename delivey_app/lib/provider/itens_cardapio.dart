524b4a757251a7866317ee76350b4b9efaf50d68
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-24 17:41:49 -0300

    Create itens_cardapio.dart

---
+++ delivey_app/lib/provider/itens_cardapio.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:delivey_app/data/data_item_cardapio.dart';
+import 'package:delivey_app/models/item_cardapio.dart';
+import 'package:flutter/cupertino.dart';
+
+class ListaItensCardapioProvider with ChangeNotifier {
+  List<ModelItemCardapio> _itens = dadosItemCardapio;
+
+
+  List<ModelItemCardapio> get itensCardapio => [..._itens];
+
+   
+}
