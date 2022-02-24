de4fe3a1984b348e6c2ff23e0497692f8300fc0f
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-24 17:42:06 -0300

    Update data_pedido.dart

--- delivey_app/lib/data/data_pedido.dart
+++ delivey_app/lib/data/data_pedido.dart
@@ -6,16 +6,5 @@ import 'package:delivey_app/views/tela_carrinho.dart';
 class DadosPedido{
     final List<ModelPedido> dadosPedidos = [];
 
-    void adicionarPedido(String pedido, List<String> adicionais, String valorTotalItem){
-          dadosPedidos.add(
-            ModelPedido(codigo: Random().nextDouble().toString(),
-            pedido: pedido, 
-            adicionais: adicionais, 
-            valorTotalItem: valorTotalItem),
-            );
-            print(dadosPedidos[0].pedido);
-            final TelaCarrinho dca = TelaCarrinho();
-            
-             
-    }
+    
 }
