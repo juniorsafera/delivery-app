6ff433111148163bc63449044bde6033259ca5d0
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-24 17:42:02 -0300

    Update main.dart

--- delivey_app/lib/main.dart
+++ delivey_app/lib/main.dart
@@ -1,20 +1,31 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:delivey_app/provider/itens_adicionais.dart';
+import 'package:delivey_app/provider/itens_cardapio.dart';
+import 'package:delivey_app/provider/itens_pedido.dart';
 import 'package:flutter/material.dart';
 import 'package:delivey_app/others/rotas.dart';
 import 'package:delivey_app/views/tela_carrinho.dart';
 import 'package:delivey_app/views/tela_inicial.dart';
 import 'package:delivey_app/views/tela_principal.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        Rotas.home: (context) => TelaInicial(),
-        Rotas.telaprincipal: (context) => TelaPrincipal(),
-        Rotas.telaCarrinho: (context) => TelaCarrinho(),
-      },
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider( create: (_) => ListaItensCardapioProvider(),),
+        ChangeNotifierProvider( create: (_) => ListaAdicionaisProvider(),),
+        ChangeNotifierProvider( create: (_) => ListaPedidosProvider(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          Rotas.home: (context) => TelaInicial(),
+          Rotas.telaprincipal: (context) => TelaPrincipal(),
+          Rotas.telaCarrinho: (context) => TelaCarrinho(),
+        },
+      ),
     ),
   );
 }
