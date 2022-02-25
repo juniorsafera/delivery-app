c96e0614c995f43305585cb93470c90d617bded5
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-25 17:02:17 -0300

    Update tela_carrinho.dart

--- delivey_app/lib/views/tela_carrinho.dart
+++ delivey_app/lib/views/tela_carrinho.dart
@@ -10,24 +10,18 @@ class TelaCarrinho extends StatefulWidget {
 
   @override
   State<TelaCarrinho> createState() => _TelaCarrinhoState();
-
- }
+}
 
 class _TelaCarrinhoState extends State<TelaCarrinho> {
-   
-   
-   
-   
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final providerPedidos = Provider.of<ListaPedidosProvider>(context);
-     final List<ModelPedido> pedidos = providerPedidos.itensPedido;
+    final List<ModelPedido> pedidos = providerPedidos.itensPedido;
 
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Carrinho' ) ,
+          title: const Text('Carrinho'),
           foregroundColor: Colors.black,
           backgroundColor: Colors.white,
           centerTitle: true,
@@ -36,70 +30,76 @@ class _TelaCarrinhoState extends State<TelaCarrinho> {
           child: Container(
               child: Column(
             children: [
-              
               ListView.builder(
-                shrinkWrap: true,
-                itemCount: pedidos.length,
-                itemBuilder: (context, index){
-                  final _pedido = pedidos[index];
-                  return 
-                 /* ListTile(
+                  shrinkWrap: true,
+                  itemCount: pedidos.length,
+                  itemBuilder: (context, index) {
+                    final _pedido = pedidos[index];
+                    return
+                        /* ListTile(
                     title: Text('1x ${_pedido.pedido.titulo}'),
                     subtitle: Text('Adicionais: ${_pedido.adicionais.toString().replaceAll('[', ' ').replaceAll(']', ' ')}'),
                     trailing: TrailingItemCarrinho(valorItem: _pedido.valorTotalItem)          
                   ); */
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text('1x ${_pedido.pedido.titulo}'),
-                            Text('Adicionais: ${_pedido.adicionais.toString().replaceAll('[', ' ').replaceAll(']', ' ')}'),
-                            Text(_pedido.observacao),
-                            Text('teste:' + _pedido.observacao),
-                             
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Row(children: [
-                              const Text('R\$ 00,00'),
-                              IconButton(onPressed: (){
-                                 providerPedidos.removerItemPedido(_pedido);
-                                 //print(_pedido.codigo);
+                        Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    '1x  ' '${_pedido.pedido.titulo}',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                
+                                Text(_pedido.adicionais.first.titulo ),
+                              
+                              
+                               Text(
+                                    _pedido.observacao,
+                                    style: TextStyle(color: Colors.red),
+                                  ),
                                  
-                              }, icon: const Icon(Icons.delete, color: Colors.red,))
-                            ],)
-                          ],
-                        )
-                      ],
-              
-                    ),
-                  );
-              
-                }
-
-                
-                
-                ),
-
-                const Text('Digite seu nome'),
-
-                FloatingActionButton.extended(
-                  onPressed: (){
-                    print(' enviando: '  + pedidos[0].pedido.titulo );
-                    print(' enviando: '  + pedidos[0].observacao );
-                  }, label:const Text('Enviar Pedido'),
-                  icon:const Icon(Icons.send),
-                  backgroundColor: Paleta.corPrimaria,
-                  
-                  )
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Text('R\$ 00,00'),
+                                      IconButton(
+                                          onPressed: () {
+                                            providerPedidos
+                                                .removerItemPedido(_pedido);
+                                          },
+                                          icon: const Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Divider(),
+                        ],
+                      ),
+                    );
+                  }),
+              const Text('Digite seu nome'),
+              FloatingActionButton.extended(
+                onPressed: () {},
+                label: const Text('Enviar Pedido'),
+                icon: const Icon(Icons.send),
+                backgroundColor: Paleta.corPrimaria,
+              )
             ],
           )),
-
-          
         ));
   }
 }
