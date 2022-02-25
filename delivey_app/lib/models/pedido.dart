66627815c644926ee6b9714596162f0a2a512877
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-25 15:44:25 -0300

    Update pedido.dart

--- delivey_app/lib/models/pedido.dart
+++ delivey_app/lib/models/pedido.dart
@@ -4,12 +4,14 @@ class ModelPedido {
   final String codigo;
   final ModelItemCardapio pedido;
   final List<String> adicionais;
+  final String observacao;
   final String valorTotalItem;
 
   const ModelPedido({
     required this.codigo,
     required this.pedido,
     required this.adicionais,
+    required this.observacao,
     required this.valorTotalItem,
   });
 }
