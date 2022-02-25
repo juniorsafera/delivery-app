f5c96c96a55707a65ed8a3b2ad23ad915d35ef9e
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-25 17:02:20 -0300

    Update itens_adicionais.dart

--- delivey_app/lib/provider/itens_adicionais.dart
+++ delivey_app/lib/provider/itens_adicionais.dart
@@ -4,10 +4,31 @@ import 'package:flutter/material.dart';
 
 class ListaAdicionaisProvider with ChangeNotifier {
   List<ModelItemAdicional> _itens = dadosItensAdicionais;
+   List<ModelItemAdicional> _itensSelecionados = [];
 
 
   List<ModelItemAdicional> get itensAdicionais => [..._itens];
+  List<ModelItemAdicional> get itensSelecionados => [..._itensSelecionados];
 
+
+ double get ValorTotalItensSelecionados {
+    double valor = 0.0;
+    _itensSelecionados.forEach((element) {
+      valor += double.parse(element.valor);
+    });
+    return valor;
+  }
+
+  void adicionarItemAdicional(ModelItemAdicional item) {
+    _itensSelecionados.add(item);
+    notifyListeners();
+  }
+  void removerItemAdicional(ModelItemAdicional item) {
+    // ignore: list_remove_unrelated_type
+    _itensSelecionados.remove(item);
+    notifyListeners();
+     
+  }
 
   
    
