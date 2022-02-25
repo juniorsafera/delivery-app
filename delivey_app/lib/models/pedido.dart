b735f829d7adf4f34879008af3031c0784b1e3de
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-25 17:02:23 -0300

    Update pedido.dart

--- delivey_app/lib/models/pedido.dart
+++ delivey_app/lib/models/pedido.dart
@@ -1,9 +1,10 @@
+import 'package:delivey_app/models/item_adicional.dart';
 import 'package:delivey_app/models/item_cardapio.dart';
 
 class ModelPedido {
   final String codigo;
   final ModelItemCardapio pedido;
-  final List<String> adicionais;
+  final List<ModelItemAdicional> adicionais;
   final String observacao;
   final String valorTotalItem;
 
