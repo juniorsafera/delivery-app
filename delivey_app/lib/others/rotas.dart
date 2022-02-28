efa1b630c311585e59b922350704d52e1840671f
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-28 12:57:51 -0300

    Update rotas.dart

--- delivey_app/lib/others/rotas.dart
+++ delivey_app/lib/others/rotas.dart
@@ -2,5 +2,6 @@ class Rotas {
   static const home = '/';
   static const telaprincipal = '/telaprincipal';
   static const telaCarrinho = '/telacarrinho';
+  static const telaAdicionarCarrinho = '/tela-adicionar-carrinho';
   
 }
