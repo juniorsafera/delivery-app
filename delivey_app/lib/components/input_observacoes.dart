489ca406870d32eb34e42fec28eab99bdb354d58
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-01 17:30:59 -0300

    Create input_observacoes.dart

---
+++ delivey_app/lib/components/input_observacoes.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class InputObservacoes extends StatelessWidget {
+  final TextEditingController textObservacao;
+  const InputObservacoes({Key? key, required this.textObservacao})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Container(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: const Text(
+                "Observações:",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              )),
+
+          // Definindo   input (Observações)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.grey)),
+                  child: TextField(
+                    controller: textObservacao,
+                    maxLines: 3,
+                    decoration: const InputDecoration.collapsed(
+                      hintText: " ",
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
