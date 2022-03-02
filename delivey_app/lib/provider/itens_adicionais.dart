664dae29e40fe465e5cfd9fc371c1d4643e696d0
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-01 22:11:44 -0300

    Update itens_adicionais.dart

--- delivey_app/lib/provider/itens_adicionais.dart
+++ delivey_app/lib/provider/itens_adicionais.dart
@@ -5,34 +5,57 @@ import 'package:flutter/material.dart';
 class ListaAdicionaisProvider with ChangeNotifier {
   List<ModelItemAdicional> _itens = dadosItensAdicionais;
   List<ModelItemAdicional> _itensSelecionados = [];
+  double valor = 0;
 
   List<ModelItemAdicional> get itensAdicionais => [..._itens];
   List<ModelItemAdicional> get itensSelecionados => [..._itensSelecionados];
-
+  double get vlt => valor;
+  
   double get ValorTotalItensSelecionados {
-    double valor = 0;
+    double valor2 = 0;
+
+    _itensSelecionados.forEach((element) {
+      valor2 += double.parse(element.valor);
+    });
+    return valor2;
+  }
+
+  somarValor(ModelItemAdicional item) {
     _itensSelecionados.forEach((element) {
       valor += double.parse(element.valor);
     });
     return valor;
-    notifyListeners();
+  }
+
+  subtrairValor(ModelItemAdicional item) {
+    _itensSelecionados.forEach((element) {
+      valor -= double.parse(element.valor);
+    });
+    return valor;
   }
 
   void adicionarItemAdicional(ModelItemAdicional item) {
     _itensSelecionados.add(item);
-    print(ValorTotalItensSelecionados);
-    notifyListeners();
-  }
-
-  void removerItemAdicional(ModelItemAdicional item) {
-    // ignore: list_remove_unrelated_type
-    _itensSelecionados.remove(item);
+    somarValor(item);
     print(_itensSelecionados.length);
     notifyListeners();
   }
 
+  void removerItemAdicional(ModelItemAdicional item) {
+    _itensSelecionados.remove(item);
+    subtrairValor(item);
+    print(_itensSelecionados.toList().length);
+    notifyListeners();
+  }
+
+  void deletar(ModelItemAdicional value) {
+    _itensSelecionados.remove(value);
+    print(_itensSelecionados.toList().length);
+  }
+
   void limpar() {
     _itensSelecionados = [];
+    valor = 0;
     notifyListeners();
   }
 }
