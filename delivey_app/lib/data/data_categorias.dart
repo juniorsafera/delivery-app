cc0e636abc936bcd2eb45437ee0f9b6373546534
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-03 11:32:49 -0300

    Update data_categorias.dart

--- delivey_app/lib/data/data_categorias.dart
+++ delivey_app/lib/data/data_categorias.dart
@@ -19,24 +19,5 @@ const dadosCategorias = [
     id: 'c4',
     titulo: 'Bebidas',
   ),
-  ModelCategoria(
-    id: 'c5',
-    titulo: 'outros1',
-  ),
-  ModelCategoria(
-    id: 'c6',
-    titulo: 'outros2',
-  ),
-  ModelCategoria(
-    id: 'c7',
-    titulo: 'outros3',
-  ),
-  ModelCategoria(
-    id: 'c8',
-    titulo: 'outros4',
-  ),
-  ModelCategoria(
-    id: 'c9',
-    titulo: 'outros5',
-  ),
+   
 ];
