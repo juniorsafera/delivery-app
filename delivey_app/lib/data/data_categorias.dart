9b127ac0c89977b6cd918b1ffd4d20bcb3c1ce15
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-02 23:44:50 -0300

    Create data_categorias.dart

---
+++ delivey_app/lib/data/data_categorias.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:delivey_app/models/categorias.dart';
+
+const dadosCategorias = [
+  ModelCategoria(
+    id: 'c1',
+    titulo: 'Hamburguers',
+  ),
+  ModelCategoria(
+    id: 'c2',
+    titulo: 'Hot-Dogs',
+  ),
+  ModelCategoria(
+    id: 'c3',
+    titulo: 'Combos',
+  ),
+  ModelCategoria(
+    id: 'c4',
+    titulo: 'Bebidas',
+  ),
+  ModelCategoria(
+    id: 'c5',
+    titulo: 'outros1',
+  ),
+  ModelCategoria(
+    id: 'c6',
+    titulo: 'outros2',
+  ),
+  ModelCategoria(
+    id: 'c7',
+    titulo: 'outros3',
+  ),
+  ModelCategoria(
+    id: 'c8',
+    titulo: 'outros4',
+  ),
+  ModelCategoria(
+    id: 'c9',
+    titulo: 'outros5',
+  ),
+];
