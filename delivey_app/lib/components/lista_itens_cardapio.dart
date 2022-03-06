b212d10c3a1e8a3a7f1640a712093dceeb6fa48f
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-06 11:37:38 -0300

    Update lista_itens_cardapio.dart

--- delivey_app/lib/components/lista_itens_cardapio.dart
+++ delivey_app/lib/components/lista_itens_cardapio.dart
@@ -1,14 +1,9 @@
-import 'dart:math';
 
-import 'package:delivey_app/models/categorias.dart';
 import 'package:delivey_app/others/rotas.dart';
-import 'package:delivey_app/provider/itens_pedido.dart';
 import 'package:flutter/material.dart';
 import 'package:delivey_app/components/botaoadd.dart';
 import 'package:delivey_app/models/item_cardapio.dart';
-import 'package:delivey_app/models/pedido.dart';
 import 'package:delivey_app/others/paleta_cores.dart';
-import 'package:provider/provider.dart';
 
 class ListaItemCardapio extends StatefulWidget {
   final List<ModelItemCardapio> itens;
@@ -27,48 +22,45 @@ class ListaItemCardapio extends StatefulWidget {
 class _ListaItemCardapioState extends State<ListaItemCardapio> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: widget.itens.length,
-        itemBuilder: (context, index) {
-          final List<ModelItemCardapio> lista = widget.itens
-              .where((e) =>
-                  e.categorias.contains(widget.categoria)   )
-              .toList();
-          final _item = lista[index];
-          return Column(
-            children: [
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () => Navigator.of(context)
-                      .pushNamed(Rotas.telaAdicionarCarrinho, arguments: _item),
-                  child: ListTile(
-                    title: Text(
-                      _item.titulo,
-                      style: const TextStyle(
-                        color: Paleta.corPrimaria,
-                        fontWeight: FontWeight.bold,
-                      ),
+    final List<ModelItemCardapio> listaItens = widget.itens
+        .where((e) => e.categorias.contains(widget.categoria))
+        .toList();
+    return ListView.builder(
+      itemCount: listaItens.length,
+      itemBuilder: (context, index) {
+        final _item = listaItens[index];
+        return Column(
+          children: [
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () => Navigator.of(context)
+                    .pushNamed(Rotas.telaAdicionarCarrinho, arguments: _item),
+                child: ListTile(
+                  title: Text(
+                    _item.titulo,
+                    style: const TextStyle(
+                      color: Paleta.corPrimaria,
+                      fontWeight: FontWeight.bold,
                     ),
-                    subtitle: Text(
-                      _item.itens
-                          .toString()
-                          .replaceAll('[', '')
-                          .replaceAll(']', ''),
-                      style: const TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    trailing: BotaoAdd(valor: _item.valor),
                   ),
+                  subtitle: Text(
+                    _item.itens
+                        .toString()
+                        .replaceAll('[', '')
+                        .replaceAll(']', ''),
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  trailing: BotaoAdd(valor: _item.valor),
                 ),
               ),
-              const Divider(),
-            ],
-          );
-        },
-      ),
+            ),
+            const Divider(),
+          ],
+        );
+      },
     );
   }
 }
