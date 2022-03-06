56cedded80749330e1b9ff141cede168cd1494b4
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-06 11:36:33 -0300

    Update itens_cardapio.dart

--- delivey_app/lib/provider/itens_cardapio.dart
+++ delivey_app/lib/provider/itens_cardapio.dart
@@ -1,5 +1,7 @@
 
 
+// ignore_for_file: prefer_final_fields
+
 import 'package:delivey_app/data/data_item_cardapio.dart';
 import 'package:delivey_app/models/item_cardapio.dart';
 import 'package:flutter/cupertino.dart';
