4674f039aa58fe4db2690e0a9525eb6f49c68ffa
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-06 11:36:52 -0300

    Update data_pedido.dart

--- delivey_app/lib/data/data_pedido.dart
+++ delivey_app/lib/data/data_pedido.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
 
 import 'package:delivey_app/models/pedido.dart';
-import 'package:delivey_app/views/tela_carrinho.dart';
 
 class DadosPedido{
     final List<ModelPedido> dadosPedidos = [];
