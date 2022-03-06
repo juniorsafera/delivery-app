730b5392edbaa0bc33cd990198c2537d93ad5a19
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-06 11:37:20 -0300

    Update trailing_item_carrinho.dart

--- delivey_app/lib/components/trailing_item_carrinho.dart
+++ delivey_app/lib/components/trailing_item_carrinho.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
 import 'package:flutter/material.dart';
 
 class TrailingItemCarrinho extends StatelessWidget {
@@ -8,13 +10,13 @@ class TrailingItemCarrinho extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 100,
       height: 50,
       child: Row(
                       children: [
                         Text('R\$ ${valorItem},00'),
-                        IconButton(onPressed: (){}, icon: Icon(Icons.delete))
+                        IconButton(onPressed: (){}, icon:const Icon(Icons.delete))
                       ],
                     )
                     );
