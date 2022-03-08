bcc74422db53724f83ed12df3f566c2e9ea2a071
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-08 11:22:08 -0300

    Update tela_adiconar_carrinho.dart

--- delivey_app/lib/views/tela_adiconar_carrinho.dart
+++ delivey_app/lib/views/tela_adiconar_carrinho.dart
@@ -22,8 +22,10 @@ class TelaAdicionarCarrinho extends StatefulWidget {
 }
 
 class _TelaAdicionarCarrinhoState extends State<TelaAdicionarCarrinho> {
-  double totalItem = 0;
+  final TextEditingController textObservacao = TextEditingController();
 
+  double totalItem = 0;
+  
   @override
   Widget build(BuildContext context) {
     final itemCardapio =
@@ -34,7 +36,7 @@ class _TelaAdicionarCarrinhoState extends State<TelaAdicionarCarrinho> {
     final _itensAdicionais =
         Provider.of<ListaAdicionaisProvider>(context, listen: false);
 
-    final TextEditingController textObservacao = TextEditingController();
+    
 
     final size = MediaQuery.of(context).size;
     final id = Random.secure();
@@ -53,115 +55,117 @@ class _TelaAdicionarCarrinhoState extends State<TelaAdicionarCarrinho> {
           backgroundColor: Colors.white,
           foregroundColor: Colors.black),
       body: SafeArea(
-        child: SizedBox(
-          width: size.width,
-          height: size.height,
-          //padding: const EdgeInsets.all(30),
-          child: Column(
-            children: [
-              SizedBox(
-                height: size.height - 57,
-                child: Container(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                        child: Container(),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            itemCardapio.titulo,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: size.width,
+            height: size.height,
+            //padding: const EdgeInsets.all(30),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height - 57,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                          child: Container(),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              itemCardapio.titulo,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              'R\$ ' + itemCardapio.valor + ',00',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(),
+                        ),
+              
+                        // Definindo área de adicionais
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            onTap: () {
+                              setState(() {
+                                totalItem++;
+                              });
+                            },
+                            child: const Text(
+                              'Adicionais',
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                           ),
-                          Text(
-                            'R\$ ' + itemCardapio.valor + ',00',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: ListaItensAdicionais(
+                              itensAdicionais: _itensAdicionais.itensAdicionais,
+                              valorTotal: _valorTotalItemAdicional,
                             ),
                           ),
-                        ],
-                      ),
-                      const Divider(),
-                      SizedBox(
-                        height: 15,
-                        child: Container(),
-                      ),
-      
-                      // Definindo área de adicionais
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: InkWell(
-                          onTap: () {
-                            setState(() {
-                              totalItem++;
-                            });
-                          },
-                          child: const Text(
-                            'Adicionais',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: ListaItensAdicionais(
-                            itensAdicionais: _itensAdicionais.itensAdicionais,
-                            valorTotal: _valorTotalItemAdicional,
+              
+                        const Divider(),
+              
+                        Expanded(
+                            flex: 1,
+                            child: SizedBox(
+                                width: size.width * 0.9,
+                                child: SingleChildScrollView(
+                                  child: InputObservacoes(
+                                      textObservacao: textObservacao),
+                                ))),
+              
+                        // Definindo label de input (Observações)
+              
+                         
+              
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: FloatingActionButton.extended(
+                            backgroundColor: Paleta.corSecundaria,
+                            onPressed: () {
+                              providerPedido.adicionarItemPedido(ModelPedido(
+                                codigo: id.nextDouble().toString(),
+                                pedido: itemCardapio,
+                                adicionais: _itensAdicionais.itensSelecionados,
+                                observacao: textObservacao.text,
+                                valorTotalItem: total.toString(),
+                              ));
+                              Navigator.pop(context);
+                              _itensAdicionais.limpar();
+                            },
+                            icon: const Icon(Icons.shopping_cart),
+                            label: Text(
+                              'Adicionar ao carrinho R\$ ' +
+                                  total.toString() +
+                                  ',00',
+                            ),
                           ),
-                        ),
-                      ),
-      
-                      const Divider(),
-      
-                      Expanded(
-                          flex: 1,
-                          child: SizedBox(
-                              width: size.width * 0.9,
-                              child: SingleChildScrollView(
-                                child: InputObservacoes(
-                                    textObservacao: textObservacao),
-                              ))),
-      
-                      // Definindo label de input (Observações)
-      
-                       
-      
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: FloatingActionButton.extended(
-                          backgroundColor: Paleta.corSecundaria,
-                          onPressed: () {
-                            providerPedido.adicionarItemPedido(ModelPedido(
-                              codigo: id.nextDouble().toString(),
-                              pedido: itemCardapio,
-                              adicionais: _itensAdicionais.itensSelecionados,
-                              observacao: textObservacao.text,
-                              valorTotalItem: total.toString(),
-                            ));
-                            Navigator.pop(context);
-                            _itensAdicionais.limpar();
-                          },
-                          icon: const Icon(Icons.shopping_cart),
-                          label: Text(
-                            'Adicionar ao carrinho R\$ ' +
-                                total.toString() +
-                                ',00',
-                          ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
