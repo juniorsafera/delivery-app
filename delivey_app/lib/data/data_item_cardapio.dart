bb2b5aa932aafe029d4fb09d6ce67903a1423dab
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-08 02:33:54 -0300

    Update data_item_cardapio.dart

--- delivey_app/lib/data/data_item_cardapio.dart
+++ delivey_app/lib/data/data_item_cardapio.dart
@@ -4,12 +4,16 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '001',
       titulo: 'X-BURGUER',
+      urlImagem:
+          'https://images.unsplash.com/photo-1572448862527-d3c904757de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YnVyZ2Vyc3xlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60/800x800',
       itens: ['pão', 'hambúrguer', 'maionese artesenal', 'queijo', 'presunto'],
       categorias: ['c1'],
       valor: '14'),
   ModelItemCardapio(
       codigo: '002',
       titulo: 'X-SALADA',
+      urlImagem:
+          'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80/800x800',
       itens: [
         'pão',
         'hambúrguer',
@@ -21,6 +25,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '003',
       titulo: 'X-EGG',
+      urlImagem:
+          'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80/800x800',
       itens: [
         'pão',
         'hambúrguer',
@@ -39,6 +45,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '004',
       titulo: 'X-CALABRESA',
+      urlImagem:
+          'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80/800x800',
       itens: [
         'pão',
         'hambúrguer',
@@ -57,6 +65,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '005',
       titulo: 'X-BACON',
+      urlImagem:
+          'https://images.unsplash.com/photo-1572448862527-d3c904757de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80/800x800',
       itens: [
         'pão',
         'hambúrguer',
@@ -75,6 +85,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '006',
       titulo: 'X-GALINHA',
+      urlImagem:
+          'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80/800x800',
       itens: [
         'pão',
         'filé de frango',
@@ -92,6 +104,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '007',
       titulo: 'X-FILÉ',
+      urlImagem:
+          'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80/800x800',
       itens: [
         'pão',
         'filé',
@@ -109,6 +123,8 @@ final dadosItemCardapio = [
   ModelItemCardapio(
       codigo: '008',
       titulo: 'Hot-Dog Frango',
+      urlImagem:
+          'https://images.unsplash.com/photo-1619740455993-9e612b1af08a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/800x800',
       itens: [
         'pão',
         'filé',
@@ -125,13 +141,17 @@ final dadosItemCardapio = [
       valor: '14'),
   ModelItemCardapio(
       codigo: '009',
-      titulo: 'Coca-Cola 1L',
+      titulo: 'Coca-Cola 250ml',
+      urlImagem:
+          'https://images.unsplash.com/photo-1514178255089-603d3a35b24a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80/800x800',
       itens: [],
       categorias: ['c4'],
-      valor: '6'),
+      valor: '4'),
   ModelItemCardapio(
       codigo: '010',
       titulo: 'X-FILÉ Combo + Fritas',
+      urlImagem:
+          'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y29tYm8lMjBidXJnZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60/800x800',
       itens: [
         'pão',
         'filé',
